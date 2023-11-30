a2ddc56ead07e532fddb3676886d9cadc85f48a4
Shrey K <srkkadia@gmail.com>
2023-11-30 16:02:42 +0530

    Logical, ternary Operators

--- FIrst_Program.c
+++ FIrst_Program.c
@@ -16,19 +16,19 @@ void main() {
     scanf("%d", &b);
 
     //Arithmetic Operators
-    printf("%d + %d = %d\n", a, b, a+b);
-    printf("%d - %d = %d\n", a, b, a-b);
-    printf("%d * %d = %d\n", a, b, a*b);
-    printf("%d / %d = %d\n", a, b, a/b);  //return quotient
-    printf("%d (mod) %d = %d\n", a, b, a%b);  //return remainder
+    // printf("%d + %d = %d\n", a, b, a+b);
+    // printf("%d - %d = %d\n", a, b, a-b);
+    // printf("%d * %d = %d\n", a, b, a*b);
+    // printf("%d / %d = %d\n", a, b, a/b);  //return quotient
+    // printf("%d (mod) %d = %d\n", a, b, a%b);  //return remainder
 
     //Relational Operators (returns 0 or 1)
-    printf("%d == %d = %d\n", a, b, a==b);  //equal to
-    printf("%d != %d = %d\n", a, b, a!=b);  //not equal to
-    printf("%d > %d = %d\n", a, b, a>b);    //greater than
-    printf("%d < %d = %d\n", a, b, a<b);    //less than
-    printf("%d >= %d = %d\n", a, b, a>=b);  //greater than or equal to
-    printf("%d <= %d = %d\n", a, b, a<=b);  //less than or equal to
+    // printf("%d == %d = %d\n", a, b, a==b);  //equal to
+    // printf("%d != %d = %d\n", a, b, a!=b);  //not equal to
+    // printf("%d > %d = %d\n", a, b, a>b);    //greater than
+    // printf("%d < %d = %d\n", a, b, a<b);    //less than
+    // printf("%d >= %d = %d\n", a, b, a>=b);  //greater than or equal to
+    // printf("%d <= %d = %d\n", a, b, a<=b);  //less than or equal to
 
     //Assignment Operators (assign value of right operand to left operand)
     // a = 10-1;
@@ -36,6 +36,47 @@ void main() {
     // printf("a = %d\n", a);
 
     // system("pause"); //Pauses the console screen
+
+
+    //Conditional/Ternary Operator
+    // (condition)? side 1(true) : side 2 (false)
+    // (a>b)?printf("\n%d is greater than %d", a, b) : printf("\n%d is greater than %d", b, a);
+
+
+    // Logical Operators (1 means true and 0 means false)
+    // && (Logical AND), ||(Logical OR), !(Logical NOT)
+    /*
+        AND (if both conditions are true then only the result is true)
+
+        cond1  cond2   cond1 && cond2
+        0       0       0
+        0       1       0
+        1       0       0
+        1       1       1
+
+        OR (if any one of the condition is true then the result is true)
+
+        cond1  cond2   cond1 || cond2
+        0       0       0
+        0       1       1
+        1       0       1
+        1       1       1
+
+
+        NOT (if the condition is true then the result is false and vice versa)
+
+        cond1   !cond1
+        0       1
+        1       0
+
+    */
+
+    // printf("> %d", (a>b) && (a!=b));
+    // printf("> %d", a >= 0 && a <= 10);  //this means a is between 0 and 10 (both inclusive)
+    // printf("> %d", a > b || a < b);
+    // printf("> %d", !(a==b));
+
+
 }
 
 /*
@@ -55,5 +96,16 @@ Operand and Operator:
 
 Operand: The data on which the operation is performed
 Operator: The symbol that performs the operation
+
+HomeWork:
+
+10. Decimal 155 -> Binary 10011011
+11. Decimal 167 -> Binary 10100111
+12. Decimal 182 -> Binary 10110110
+13. Decimal 198 -> Binary 11000110
+14. Decimal 213 -> Binary 11010101
+15. Decimal 226 -> Binary 11100010
+
+https://jamboard.google.com/d/1l1ZI3amOUYcrE1ofRtm1b_JPfwwWOroBPXl5F6JBeJA/viewer?mtt=cvarjy2mg4l4&f=1
 
 */
