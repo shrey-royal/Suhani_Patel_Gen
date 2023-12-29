220d72b4c739df7abcc8b2bb847fe8c2739c57d3
Shrey K <srkkadia@gmail.com>
2023-12-29 17:02:50 +0530

    fibonacci series

--- Control Statements/ForExamples.c
+++ Control Statements/ForExamples.c
@@ -27,25 +27,39 @@ void main() {
 
 // --------------------------------------------------------------------------------------------
 // -armstrong number
-    int num, temp, sum=0, rem;
+    // int num, temp, sum=0, rem;
 
-    printf("\nEnter any integer: ");
-    scanf("%d", &num);
+    // printf("\nEnter any integer: ");
+    // scanf("%d", &num);
 
-    temp = num; //copy generated
+    // temp = num; //copy generated
 
-    for(;num>0;) {
-        rem = num % 10;             //getting last digit
-        sum = sum + rem*rem*rem;    //adding the cube of the last digit into the sum variable
-        num = num / 10;             //remove last digit from the number
-        // printf("\nsum = %d\trem = %d", sum, rem);
+    // for(;num>0;) {
+    //     rem = num % 10;             //getting last digit
+    //     sum = sum + rem*rem*rem;    //adding the cube of the last digit into the sum variable
+    //     num = num / 10;             //remove last digit from the number
+    //     // printf("\nsum = %d\trem = %d", sum, rem);
+    // }
+
+    // if(sum == temp) {
+    //     printf("\n%d is an Armstrong Number", temp);
+    // } else {
+    //     printf("\n%d is not an Armstrong Number", temp);
+    // }
+
+// --------------------------------------------------------------------------------------------
+// -fibonacci series
+    int i, n1=0, n2=1, n3, terms;
+    printf("\nEnter terms: ");
+    scanf("%d", &terms);
+
+    printf("Fibonacci Series: %d, %d, ", n1, n2);
+    n3 = n1+n2;
+    for(i=0; i<terms-2; i++) {
+        printf("%d, ", n3);
+        n1 = n2;
+        n2 = n3;
+        n3 = n1+n2;
     }
-
-    if(sum == temp) {
-        printf("\n%d is an Armstrong Number", temp);
-    } else {
-        printf("\n%d is not an Armstrong Number", temp);
-    }
-
 }
 
